ac071b65fa35df18e8cddc744741d4b870685855
tasuyfga <67712341+tatesheehy@users.noreply.github.com>
2026-07-13 21:53:59 -0700

    k

--- running-site/js/rankings.js
+++ running-site/js/rankings.js
@@ -912,7 +912,7 @@ function buildRankingsDetail(eventName, opts = {}) {
             </div>
             <div class="rd-header-btns">
               <a class="rd-compare-btn" href="event-tracker.html?event=${encodeURIComponent(eventName)}">Event Tracker</a>
-              <button class="rd-compare-btn" onclick="openH2H(null,'${eventName.replace(/'/g,"\\'")}')">Compare Athletes</button>
+              <button class="rd-compare-btn rd-compare-btn--primary" onclick="openH2H(null,'${eventName.replace(/'/g,"\\'")}')">Compare Athletes</button>
             </div>
           </div>
         </div>
